965a349851490da87a8e20c6f1b67babcb043423
jiwon79 <19-079@ksa.hs.kr>
2023-12-28 01:24:33 +0900

    :sparkles: add desktop file

---
+++ src/view/pages/desktop/views/Desktop.tsx
@@ -0,0 +1,13 @@
+import { Window } from '../../../../domain/window';
+
+export function Desktop() {
+  return (
+    <>
+      <p>MENU</p>
+      <Window />
+      <Window />
+      <Window />
+      <p>DOCK</p>
+    </>
+  );
+}
