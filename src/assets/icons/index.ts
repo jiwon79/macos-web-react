205e3a25b48317ebaf7ffaf2cd96b9c7b04b04b7
jiwon79 <19-079@ksa.hs.kr>
2023-12-28 15:46:47 +0900

    :sparkles: add window control icons

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,3 @@
+export { default as IconWindowClose } from './ic_window_close.svg';
+export { default as IconWindowMaximize } from './ic_window_maximize.svg';
+export { default as IconWindowMinimize } from './ic_window_minimize.svg';
--- src/domain/window/views/WindoeControl/WindowControl.tsx
+++ src/domain/window/views/WindoeControl/WindowControl.tsx
@@ -1,8 +1,16 @@
 import { container } from './WindowControl.css.ts';
+import {
+  IconWindowClose,
+  IconWindowMaximize,
+  IconWindowMinimize,
+} from '../../../../assets';
 
 export function WindowControl() {
   return (
     <div className={container}>
+      <IconWindowClose />
+      <IconWindowMinimize />
+      <IconWindowMaximize />
       <p>WindowControlMenu</p>
     </div>
   );
