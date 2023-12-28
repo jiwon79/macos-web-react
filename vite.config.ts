e14d7f93e82df0336d405a57a1fbf682875ef0d9
jiwon79 <19-079@ksa.hs.kr>
2023-12-28 15:46:37 +0900

    :construction_worker: add svg import config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite';
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 import react from '@vitejs/plugin-react-swc';
+import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-  plugins: [react(), vanillaExtractPlugin()],
+  plugins: [
+    react(),
+    vanillaExtractPlugin(),
+    svgr({
+      include: '**/*.svg',
+    }),
+  ],
 });
