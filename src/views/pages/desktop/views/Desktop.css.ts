40ad4a1f1fcb8de2c8aa1cc6dc309d8093d9b177
Lee Jiwon <jiiwon79@gmail.com>
2024-03-24 17:02:23 +0900

    fix: Not change wallpapaer bug when color-theme changed (#14)
    * fix: dark mode wallpaper
    
    * fix: delete console

--- src/views/pages/desktop/views/Desktop.css.ts
+++ src/views/pages/desktop/views/Desktop.css.ts
@@ -1,7 +1,13 @@
-import { style } from '@vanilla-extract/css';
+import { globalStyle, style } from '@vanilla-extract/css';
+import { darkModeSelector } from 'utils/broswer';
 
 export const desktop = style({
   position: 'relative',
   width: '100%',
   height: '100%',
+  backgroundImage: 'url(/src/assets/wallpapers/wallpaper_light.png)',
+});
+
+globalStyle(`${darkModeSelector} ${desktop}`, {
+  backgroundImage: 'url(/src/assets/wallpapers/wallpaper_dark.png)',
 });
