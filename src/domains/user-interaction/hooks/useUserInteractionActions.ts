08a138cfa1eea9063e0103d46e7be3575709baa5
jiwon79 <19-079@ksa.hs.kr>
2024-05-29 01:00:40 +0900

    refactor: delete unused files

--- src/domains/user-interaction/hooks/useUserInteractionActions.ts
+++
@@ -1,5 +0,0 @@
-import { useUserInteractionStore } from './useUserInteractionStore.ts';
-
-export function useUserInteractionActions() {
-  return useUserInteractionStore((state) => state.actions);
-}
